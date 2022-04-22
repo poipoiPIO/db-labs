40515333383cded1c213469b4fefb1a65c7c6f54
Lappely <lappee@yahoo.com>
2022-04-22 11:18:00 +0300

    laba3 solved

--- laba2.sql
+++ laba2.sql
@@ -1,6 +1,6 @@
 CREATE TABLE if not EXISTS Группа (
-  нг    varchar(3) UNIQUE PRIMARY Key NOT NULL, -- номер группы
-  кол   integer NULL, -- количество студентов в группе
+  нг    VARCHAR(3) UNIQUE PRIMARY Key NOT NULL, -- номер группы
+  кол   INTEGER NULL, -- количество студентов в группе
   пбалл REAL NULL,  -- проходной балл
   
   CONSTRAINT проверка_количества -- Условие для столбца кол
@@ -11,64 +11,64 @@ CREATE TABLE if not EXISTS Группа (
 );
 
 CREATE table if not EXISTS Студент (
-  нг    varchar(3) unique NOT NULL, -- номер группы
-  нс    varchar(2) NOT NULL, -- номер студента в группе
-  фио   varchar(15) NOT NULL, -- фио
-  годр  integer NULL, -- год рождения
-  адрес varchar(25) NULL,
-  пбал  real NULL, -- проходной балл
+  нг    VARCHAR(3) NOT NULL, -- номер группы
+  нс    VARCHAR(2) NOT NULL, -- номер студента в группе
+  фио   VARCHAR(15) NOT NULL, -- фио
+  годр  INTEGER NULL, -- год рождения
+  адрес VARCHAR(25) NULL,
+  пбал  REAL NULL, -- проходной балл
   
   PRIMARY KEY (нг, нс)
 );
 
 CREATE TABLE if not EXISTS Кафедра (
-  ккаф Varchar(2)UNIQUE NOT NULL, -- код кафедры
-  нкаф varchar(15) NULL, -- название
-  тел  varchar(9) NULL, -- телефон
-  зав  varchar(15) NULL, -- фио заведущего кафедры
-  фото blob NULL -- фотография заведующего
+  ккаф VARCHAR(2) UNIQUE NOT NULL, -- код кафедры
+  нкаф VARCHAR(15) NULL, -- название
+  тел  VARCHAR(9) NULL, -- телефон
+  зав  VARCHAR(15) NULL, -- фио заведущего кафедры
+  фото BLOB NULL -- фотография заведующего
 );
 
 CREATE TABLE if not EXISTS Преподаватель (
-  табн varchar(4) UNIQUE NOT NULL, -- таб. номер
-  фио  varchar(30) NOT NULL, -- фио преподователя
-  ст   varchar(15) NULL, -- ученое степень
-  зв   varchar(10) NULL, -- ученое звание
-  ккаф varchar(2) NOT NULL -- код кафедры
+  табн VARCHAR(4) UNIQUE NOT NULL, -- таб. номер
+  фио  VARCHAR(30) NOT NULL, -- фио преподователя
+  ст   VARCHAR(15) NULL, -- ученое степень
+  зв   VARCHAR(10) NULL, -- ученое звание
+  ккаф VARCHAR(2) NOT NULL -- код кафедры
 );
 
 CREATE table if not EXISTS Предмет (
-  кп    varchar(2) NOT NULL UNIQUE, -- код предмета
-  нп    varchar(15) NULL,  -- название предмета 
-  часы  integer NULL, -- всего часов
-  лек   integer NULL, -- Лекции
-  пр 	  integer NULL, -- практика
-  чс    integer NULL, -- семестров
-  прогр blob, --программа
+  кп    VARCHAR(2) NOT NULL UNIQUE, -- код предмета
+  нп    VARCHAR(15) NULL,  -- название предмета 
+  часы  INTEGER NULL, -- всего часов
+  лек   INTEGER NULL, -- Лекции
+  пр 	  INTEGER NULL, -- практика
+  чс    INTEGER NULL, -- семестров
+  прогр BLOB, --программа
   
   CONSTRAINT проверка_количества_часов -- Условие для столбца часы
   	CHECK (часы >= 0 and часы <= 300)
 );
 
 CREATE table if not EXISTS Изучение (
-  нг      Varchar(3) NOT NULL, --номер группы
-  кп      Varchar(2) NOT NULL, -- код предмета
-  табн    Varchar(4) NOT NULL, -- таб. н. преподователя (Не знаю что ето значит, но наверное так надо)
-  видз    Varchar(3) NOT NULL, -- вид занятий
-  часы    integer NULL,
-  сбаллгр real, -- средний балл по предмету
+  нг      VARCHAR(3) NOT NULL, --номер группы
+  кп      VARCHAR(2) NOT NULL, -- код предмета
+  табн    VARCHAR(4) NOT NULL, -- таб. н. преподователя (Не знаю что ето значит, но наверное так надо)
+  видз    VARCHAR(3) NOT NULL, -- вид занятий
+  часы    INTEGER NULL,
+  сбаллгр REAL, -- средний балл по предмету
   
   PRIMARY KEY (нг, кп, табн, видз)
 );
 
 CREATE table if not EXISTS Успеваемость (
-  нг     Varchar(3) NOT NULL, --номер группы
-  нс     Varchar(2) NOT NULL, -- номер студента
-  кп     Varchar(2) NOT NULL, -- код предмета
-  табн   Varchar(4) NOT NULL, -- таб. н. преподователя (Не знаю что ето значит, но наверное так надо)
-  видз   Varchar(3) NOT NULL, -- вид занятий
+  нг     VARCHAR(3) NOT NULL, --номер группы
+  нс     VARCHAR(2) NOT NULL, -- номер студента
+  кп     VARCHAR(2) NOT NULL, -- код предмета
+  табн   VARCHAR(4) NOT NULL, -- таб. н. преподователя (Не знаю что ето значит, но наверное так надо)
+  видз   VARCHAR(3) NOT NULL, -- вид занятий
   оценка INTEGER NULL,
   
-  PRIMARY KEY (нг, нс, кп, табн, видз)
+  PRIMARY KEY (нг, нс, кп, табн)
 );
 
